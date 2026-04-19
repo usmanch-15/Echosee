a060529c0426e2396739e57850a304df6e5a67eb
Areeba Ghafoor <areebaghafoor95@gmail.com>
2026-04-19 16:23:32 +0500

    Updated Supabase Keys

--- lib/core/constants/keys.dart
+++ lib/core/constants/keys.dart
@@ -1,5 +1,4 @@
 class AppKeys {
-  static const String googleWebClientId = '302949153800-uvl2rvhff931ji4nmbpq7ome1s7qpsnk.apps.googleusercontent.com';
-  
-  // You can add more keys here
+  static const String supabaseUrl = 'https://ylxelmpafdobztgooewc.supabase.co';
+static const String supabaseAnonKey = 'sb_publishable_K494v2HjtUGbtfwC6SR9SA_T1PL6Kzi';
 }
